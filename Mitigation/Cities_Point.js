cb2931d3d22050090d04aa8fc6ade6d413d4319d
jgredig <142947198+jgredig@users.noreply.github.com>
2023-12-04 16:09:39 -0700

    Add files via upload

---
+++ Mitigation/Cities_Point.js
@@ -0,0 +1,13 @@
+var citiesPoint = {
+"type": "FeatureCollection",
+"name": "Cities_Point",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID_1": 200, "NAME": "NEW CASTLE", "ST_ABBREV": "CO", "FIPS": "08045", "PLACE_FIPS": "53395", "STATUS": null, "LAT": 39.572760161252525, "LONG": -107.53644089399999 }, "geometry": { "type": "Point", "coordinates": [ -107.536451557722586, 39.572766379049519 ] } },
+{ "type": "Feature", "properties": { "OBJECTID_1": 207, "NAME": "SILT", "ST_ABBREV": "CO", "FIPS": "08045", "PLACE_FIPS": "70195", "STATUS": null, "LAT": 39.548595161137854, "LONG": -107.656176894 }, "geometry": { "type": "Point", "coordinates": [ -107.65732379416184, 39.558874660416429 ] } },
+{ "type": "Feature", "properties": { "OBJECTID_1": 209, "NAME": "RIFLE", "ST_ABBREV": "CO", "FIPS": "08045", "PLACE_FIPS": "64255", "STATUS": null, "LAT": 39.534699161537105, "LONG": -107.78312189499999 }, "geometry": { "type": "Point", "coordinates": [ -107.78313260841918, 39.534705357599414 ] } },
+{ "type": "Feature", "properties": { "OBJECTID_1": 213, "NAME": "GLENWOOD SPRINGS", "ST_ABBREV": "CO", "FIPS": "08045", "PLACE_FIPS": "30780", "STATUS": "County Seat", "LAT": 39.550539161752972, "LONG": -107.32477889499999 }, "geometry": { "type": "Point", "coordinates": [ -107.324789509381915, 39.550545392127056 ] } },
+{ "type": "Feature", "properties": { "OBJECTID_1": 262, "NAME": "CARBONDALE", "ST_ABBREV": "CO", "FIPS": "08045", "PLACE_FIPS": "12045", "STATUS": null, "LAT": 39.402210161769631, "LONG": -107.211171894 }, "geometry": { "type": "Point", "coordinates": [ -107.211182465860077, 39.402216380603264 ] } },
+{ "type": "Feature", "properties": { "OBJECTID_1": 0, "NAME": "PARACHUTE", "ST_ABBREV": "CO", "FIPS": "08045", "PLACE_FIPS": null, "STATUS": null, "LAT": 39.454035, "LONG": -108.0500325 }, "geometry": { "type": "Point", "coordinates": [ -108.053087466628483, 39.454364714974226 ] } }
+]
+}
